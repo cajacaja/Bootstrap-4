d5707230d69523d81547a00963404f5a0a8c567c
Emir Čajić <emir.cajic@edu.fit.ba>
2019-04-03 12:08:13 +0200

    second commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+$(function(){
+
+
+    var topoffset=55;
+
+    $('.navbar a[href*=\\#]:not([href=\\#])').click(function(){
+        if(location.pathname.replace(/^\//,''===this.pathname.replace(/^\//,''))&&
+            location.hostname===this.hostname)
+        {
+            var target=$(this.hash);
+            target=target.length?target:$('[name='+this.hash.slice(1)+']');
+
+            if(target.length)
+            {
+                $('html,body').animate(
+                    {
+                        scrollTop:target.offset().top-topoffset+2
+                    },
+                    600
+                );
+                return false;
+            }
+        }
+    });
+
+
+    $(window).on('activate.bs.scrollspy',function(){
+        var hash=$('.site-nav').find('a.active').attr('href');
+        if(hash!=='#page-hero')
+        {
+            $('header nav').addClass('inbody');
+        }
+        else
+        {
+            $('header nav').removeClass('inbody');
+        }
+
+        $('#page-media .layout-animation').css(
+            'visibility:hidden'
+        );
+
+        if(hash==='#page-media'){
+            $('#page-media .layout-animation').addClass('animated fadeInRight');
+        };
+
+    });
+
+    $('#site-modal').on('show.bs.modal',function(event){
+        $(this).find('.modal-content img').attr('src',$(event.relatedTarget).data('highres'));
+    });
+});
